7d6d6000954beede595fb15acf5f90ef5770ea13
Gabriel Oliveira <greewdyBlu@gmail.com>
2021-05-12 20:04:04 -0300

    atualização

--- app/view/desenvolvedores.php
+++ app/view/desenvolvedores.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>HÍGIA - Disposicao para a Saude</title>
 
     <style>
@@ -13,8 +14,7 @@
     }
     </style>
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 
 
     <link href="https://fonts.googleapis.com/css2?family=Baloo+Tamma+2:wght@600&display=swap" rel="stylesheet">
@@ -24,75 +24,65 @@
 </head>
 
 <body class="bg-white">
+<div class="container-fluid mb-4 p-1 m-2">
+    
+
+<div class="card mb-3 shadow-lg" style=" width: 49rem;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="../../img/Gabriel.jpg" alt="..." style="max-width: 277px; ">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body m-3">
+        <h5 class="card-title">Gabriel Oliveira Alves da Silva</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
 
 
-<h2 class="fs-1 text-center">Equipe Hígia</h2>
-    <section>
-        <div class="profile-area">
-            <div class="container">
+<div class="card mb-3 shadow-lg" style=" width: 49rem;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="../../img/Sergio.jpg" alt="..." style="max-width: 277px; ">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body m-3" >
+        <h5 class="card-title">Sergio</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
 
-
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="card">
-                      <div class="img1"><img src="../../img/vaun0815-lGjzLRmrI1M-unsplash.jpg"></div>
-                      <div class="img2"><img src="../../img/Gabriel.jpg"></div>
-                     
-                      <div class="main-text">
-                        <h2>Gabriel</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, inventore quo eum sint maiores explicabo accusantium aspernatur deleniti libero! Maiores, quaerat. Animi debitis sint ratione quidem labore vitae fugiat ad?</p>
-                        
-                        </div>
-                        <center><button type="button" class="btn btn-outline-primary m-2">Contato</button></center>
-                
-                        </div>
-                      
-<<<<<<< HEAD
-                    </div>
-
-                    <div class="col-md-4">
-                        <div class="card">
-                      <div class="img1"><img src="../../img/vaun0815-lGjzLRmrI1M-unsplash.jpg"></div>
-                      <div class="img2"><img src="../../img/Jose.jpg"></div>
-                        <div class="main-text">
-                        <h2>Jose</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, inventore quo eum sint maiores explicabo accusantium aspernatur deleniti libero! Maiores, quaerat. Animi debitis sint ratione quidem labore vitae fugiat ad?</p>
-                        
-                        </div>
-                        <center><button type="button" class="btn btn-outline-primary m-2">Contato</button></center>
-                
-                        </div>
-                      
-                    </div> 
-                    <div class="col-md-4">
-                        <div class="card">
-                      <div class="img1"><img src="../../img/vaun0815-lGjzLRmrI1M-unsplash.jpg"></div>
-                      <div class="img2"><img src="../../img/Sergio.jpg"></div>
-                        <div class="main-text">
-                        <h2>Sergio</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, inventore quo eum sint maiores explicabo accusantium aspernatur deleniti libero! Maiores, quaerat. Animi debitis sint ratione quidem labore vitae fugiat ad?</p>
-                        
-                        </div>
-                        <center><button type="button" class="btn btn-outline-primary m-2">Contato</button></center>
-                
-                        </div>
-                       </div>
-
-                </div>
-
-            </div>
-            
-        </div>
+<div class="card mb-3 shadow-lg" style=" width: 49rem;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="../../img/Jose.jpg" alt="..." style="max-width: 200px; ">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body m-3">
+        <h5 class="card-title">Jose</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
         <center><button class="btn btn-primary botao-volta centralizar" onclick="window.history.back()">Voltar</button>
         </center>
-    </section>
+  
 
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous">
-    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js" integrity="sha384-lpyLfhYuitXl2zRZ5Bn2fqnhNAKOAaM/0Kr9laMspuaMiZfGmfwRNFh8HlMy49eQ" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
