36fd1a191723cb06e00b3554aecc0baa5c78b714
Gabriel Oliveira <greedwyblu@gmail.com>
2021-05-25 23:03:47 -0300

    busca

--- app/view/busca.php
+++ app/view/busca.php
@@ -160,8 +160,7 @@ if (count($resultados)) {
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
   <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
 </svg><div>
-    para ver o contato do profissional precisa fazer o cadastro de cliente
-  </div>
+   você tem que fazer o cadastro de cliente</div>
 </div>
       </div>
      
@@ -210,7 +209,7 @@ if (count($resultados)) {
       <svg xmlns="http://www.w3.org/2000/svg" width="54" height="54" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
   <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
 </svg><div>
-   para ver atualização de anuncio precisa cadastrar o seu anuncío caso tenha feito faça o login novamente
+você não pode fazer comentario no seu proprio anuncío
   </div>
 </div>   <?php   } ?>
 </div>
